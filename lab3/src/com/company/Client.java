94e71edf1a0049b6e165ae6b593fdcf9a3abaedf
NetStall <vetalkep27@gmail.com>
2023-10-24 19:06:03 +0300

    add default client

--- lab3/src/com/company/Client.java
+++ lab3/src/com/company/Client.java
@@ -2,14 +2,16 @@ package com.company;
 
 public class Client {
 
-    public void CreateFactory(){
-
+    public static void createFactory(AbstractFactory factory){
+        factory.createCircle();
+        factory.createTriangle();
 
     }
 
 
 
     public static void main(String[] args) {
-	// write your code here
+        createFactory( new BlackFactory());
+        createFactory(new WhiteFactory());
     }
 }
